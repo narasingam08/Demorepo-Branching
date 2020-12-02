b767ff65d466c833877d7a98928d1519061901cb
Ubuntu <ubuntu@ip-172-31-43-209.ap-south-1.compute.internal>
2020-12-02 17:50:54 +0000

    empty java Project

---
+++ my-Firstapp/src/main/java/com/myFirst/app/App.java
@@ -0,0 +1,13 @@
+package com.myFirst.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
